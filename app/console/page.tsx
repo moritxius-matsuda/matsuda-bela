65ce3c8b0e667e0ce010721be5bd367c3068b19f
Moritz Béla Meier <moritz@moritxius.de>
2025-05-04 16:43:35 +0200

    Update page.tsx

--- app/console/page.tsx
+++ app/console/page.tsx
@@ -1,14 +1,5 @@
-import dynamic from "next/dynamic";
-import { CircularProgress, Box } from "@mui/material";
-
-const ConsoleClient = dynamic(() => import("./ConsoleClient"), {
-  ssr: false,
-  loading: () => (
-    <Box sx={{ p: 4, textAlign: "center" }}>
-      <CircularProgress />
-    </Box>
-  ),
-});
+"use client";
+import ConsoleClient from "./ConsoleClient";
 
 export default function Page() {
   return <ConsoleClient />;
