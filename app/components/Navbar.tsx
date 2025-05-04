139db2a13d98c9dbba990d2aa9682134b2c0fc46
Moritz Béla Meier <moritz@moritxius.de>
2025-05-04 11:39:44 +0200

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,9 +21,6 @@ export default function Navbar() {
           <Button color="inherit" component={Link} href="/">
             Home
           </Button>
-          <Button color="inherit" component={Link} href="/uptime">
-            Uptime
-          </Button>
           {isSignedIn && (
             <Button color="inherit" component={Link} href="/admin">
               Admin
